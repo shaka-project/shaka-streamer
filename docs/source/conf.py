f9f964550ca58c094fcc94ba5409404432578ebf
Joey Parrish <joeyparrish@google.com>
2021-07-21 13:12:18 -0700

    docs: Change the documentation theme
    The old theme did not put enough space between members.  The new theme
    does this better, but required some extra configuration for the table
    of contents in the sidebar.
    
    Change-Id: I9fc51471551a8176d2279add8087a9ca1909be07

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,13 +105,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'globaltoc_collapse': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -126,7 +128,9 @@ html_static_path = []
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+html_sidebars = {
+    '*': ['globaltoc.html', 'searchbox.html'],
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
