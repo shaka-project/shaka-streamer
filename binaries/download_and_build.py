3de3105f34d8211b88ed3c75946181d978801f98
meryacine <omar.groza@gmail.com>
2021-08-19 11:42:19 +0200

    minor edits

--- binaries/download_and_build.py
+++ binaries/download_and_build.py
@@ -4,13 +4,12 @@ import sys
 # This import is blocking until all the binaries have been downloaded.
 import download_binaries
 
-# set the sys.argv[1] to the path of the yaml output file.
-# The build script will use it to build the wheels.
+# set the sys.argv[1] to the path of the yaml output file that
+# the build script will use to build the wheels.
 sys.argv = [
     '',
     download_binaries.YAML_OUTPUT_FILE,
   ]
 
-# This import will build the binaries as python wheels
-# for each platform we support.
+# This import will build the wheels for each platform we support.
 import build
